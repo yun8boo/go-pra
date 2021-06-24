fb91f5b98a7b3578bf399892d733039b8971d2a9
yun8boo <yuma051521@gmail.com>
2021-06-24 21:02:21 +0900

    commit

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/yun8boo/rest-api-pra
 
 go 1.16
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,64 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
+
+type Article struct {
+	Id      string `json:Id`
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+var Articles []Article
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Welcome to the HomePage")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+func getArticles(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(Articles)
+}
+
+func getArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+	for _, articre := range Articles {
+		if articre.Id == key {
+			json.NewEncoder(w).Encode(articre)
+		}
+	}
+}
+
+func createNewArticle(w http.ResponseWriter, r *http.Request) {
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var article Article
+	json.Unmarshal(reqBody, &article)
+	Articles = append(Articles, article)
+	json.NewEncoder(w).Encode(article)
+}
 
 func main() {
 	fmt.Println("start")
+
+	Articles = []Article{
+		Article{Id: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		Article{Id: "2", Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+	}
+
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/articles", getArticles).Methods("GET")
+	myRouter.HandleFunc("/articles", createNewArticle).Methods("POST")
+	myRouter.HandleFunc("/articles/{id}", getArticle).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":8080", myRouter))
 }
